2d8ba18e45818f317dea190aefba6ce89b70d60e
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 10:58:30 +0700

    Update extra_math.lua

--- maths/extra_math.lua
+++ maths/extra_math.lua
@@ -1,1 +1,42 @@
-.
+module(
+ "extra_math"
+)
+
+extra_math.epsilon=1e-9
+
+function extra_math.is_approx(value,target,artifact)
+ local o=value-target
+ if(
+  o>=-artifact
+ )and(
+  o<=artifact
+ )then
+  return true
+ end
+ return false
+end
+
+function extra_math.is_zero_approx(value)
+ if extra_math.is_approx(
+  value,
+  0,
+  extra_math.epsilon
+ )then
+  return true
+ end
+ return false
+end
+
+function extra_math.sign(value)
+ if(
+  value==0
+ )then
+  return 0
+ end
+ if(
+  value>0
+ )then
+  return 1
+ end
+ return -1
+end
