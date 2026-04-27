c39d3bee8897fdb2590159885504817251d237b5
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 19:43:59 +0700

    Update extra_math.lua

--- headers/extra_math.lua
+++ headers/extra_math.lua
@@ -10,9 +10,8 @@ https://dujanhung.github.com/extra-lua-preset/blob/maths/extra_math.lua
 module(
  "extra_math"
 )
-dofile(
- "extra-lua-preset/configs/extra_math.lua"
-)
+local config_filepath="extra-lua-preset/configs/extra_math.txt"
+extra_math.epsilon=io.lines(config_filepath)
 dofile(
  "extra-lua-preset/maths/extra_math.lua"
 )
