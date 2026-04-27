a9648c139aecc32f0c0215ded0c01e05aa86ae15
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 16:03:57 +0700

    Update variant_safeguard.lua

--- safeguards/variant_safeguard.lua
+++ safeguards/variant_safeguard.lua
@@ -1,9 +1,13 @@
 function variant_safeguard.is_boolean(
  o
 )
- if type(
-  o
- )!="boolean"then
+ if
+  type(
+   o
+  )
+  !=
+  "boolean"
+ then
   return false
  end
  return true
@@ -12,9 +16,13 @@ end
 function variant_safeguard.is_number(
  o
 )
- if type(
-  o
- )!="number"then
+ if
+  type(
+   o
+  )
+  !=
+  "number"
+ then
   return false
  end
  return true
@@ -23,9 +31,13 @@ end
 function variant_safeguard.is_string(
  o
 )
- if type(
-  o
- )!="string"then
+ if
+  type(
+   o
+  )
+  !=
+  "string"
+ then
   return false
  end
  return true
@@ -34,9 +46,13 @@ end
 function variant_safeguard.is_table(
  o
 )
- if type(
-  o
- )!="table"then
+ if
+  type(
+   o
+  )
+  !=
+  "table"
+ then
   return false
  end
  return true
@@ -45,9 +61,13 @@ end
 function variant_safeguard.is_function(
  o
 )
- if type(
-  o
- )!="function"then
+ if
+  type(
+   o
+  )
+  !=
+  "function"
+ then
   return false
  end
  return true
