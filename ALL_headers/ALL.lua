adb3dd02c35e61bc12f90cc2d64ac4ec9591c93f
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 19:10:38 +0700

    Update ALL.lua

--- ALL_headers/ALL.lua
+++ ALL_headers/ALL.lua
@@ -1,4 +1,3 @@
 local header_folder_name="extra-lua-preset/headers/"
 local file_ext=".lua"
-local file_names={"variant_safeguard","extra_math"
-}
+local file_names={"variant_safeguard","extra_math","vector"}
