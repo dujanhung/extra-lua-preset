412c08f1b5d02d4090f3531f44e1f7d766db54e5
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 18:26:55 +0700

    Update extra_math.lua

--- maths/extra_math.lua
+++ maths/extra_math.lua
@@ -1,18 +1,4 @@
-function extra_math.is_approx(
- value,
- target,
- artifact
-)
- local o=value-target
- if(
-  o>=-artifact
- )and(
-  o<=artifact
- )then
-  return true
- end
- return false
-end
+function extra_math.is_approx(value,target,artifact)local o=value-target;if o>=-artifact and o<=artifact then return true end return false end
 
 function extra_math.is_zero_approx(
  value
