9aada86a4d65fd64ae0651063613389861175ee0
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 15:32:10 +0700

    Update extra_math.lua

--- headers/extra_math.lua
+++ headers/extra_math.lua
@@ -1,1 +1,15 @@
-.
+/*
+
+required decencies:
+
+https://dujanhung.github.com/extra-lua-preset/blob/safeguards/variant_safeguard.lua
+
+*/
+
+dofile(
+ "extra-lua-preset/safeguards/variant_safeguard.lua"
+)
+
+required(
+ "variant_safeguard"
+)
