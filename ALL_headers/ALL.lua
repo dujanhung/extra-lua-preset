bd7f7263b1ba11adee21e1057c71205c5678f8b1
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 19:52:40 +0700

    Update ALL.lua

--- ALL_headers/ALL.lua
+++ ALL_headers/ALL.lua
@@ -1,4 +1,1 @@
-local config_filepath="extra_lua_preset/configs/ALL_headers.txt"
-for line in io.lines(config_filepath)do
-dofile(tostring(line))
-end
+local config_filepath="extra_lua_preset/configs/ALL_headers.txt";for line in io.lines(config_filepath)do dofile(tostring(line))end
