535c373094ee91247dfa44659dd28dca6ee03615
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 14:53:06 +0700

    Update variant_safeguard.lua

--- headers/variant_safeguard.lua
+++ headers/variant_safeguard.lua
@@ -1,3 +1,11 @@
+/*
+
+required decencies:
+
+https://dujanhung.github.com/extra-lua-preset/blob/safeguards/variant_safeguard.lua
+
+*/
+
 dofile(
  "extra-lua-preset/safeguards/variant_safeguard.lua"
 )
