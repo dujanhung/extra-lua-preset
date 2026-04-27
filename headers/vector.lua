e218a88591abf3bcbf53ea008c97ef48e29bd02c
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 18:52:11 +0700

    Update vector.lua

--- headers/vector.lua
+++ headers/vector.lua
@@ -10,15 +10,12 @@ https://dujanhung.github.com/extra-lua-preset/blob/variants/vector.lua
 dofile(
  "extra-lua-preset/headers/variant_safeguard.lua"
 )
-
 required(
  "variant_safeguard"
 )
-
 module(
  "vector"
 )
-
 dofile(
  "extra-lua-preset/variants/vector.lua"
 )
