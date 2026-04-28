2b69584caeb72e8da360e930cc8777c3108a6218
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 15:11:48 +0700

    Update ALL.lua

--- orchestrator/ALL.lua
+++ orchestrator/ALL.lua
@@ -1,5 +1,5 @@
 module("extra_lua_preset")
-extra_lua_preset.script_filepaths="addons/extra_lua_preset/orchestrator/script_filepaths.txt"
-for line in io.lines(extra_lua_preset.script_filepaths)do
+extra_lua_preset.script_filepath_root="addons/extra_lua_preset/"
+for line in do
  dofile(tostring(line))
 end
