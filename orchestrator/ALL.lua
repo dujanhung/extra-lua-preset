7a454f04ca086dc003154d0e52150d9356be33f3
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 15:19:10 +0700

    Update ALL.lua

--- orchestrator/ALL.lua
+++ orchestrator/ALL.lua
@@ -6,6 +6,4 @@ extra_lua_preset.presets={
 "variants/vector/"
 }
 extra_lua_preset.script_file="script.lua"
-for line in do
- dofile(tostring(line))
-end
+for i in extra_lua_preset.presets do dofile(extra_lua_preset.file_root..extra_lua_preset.presets[i]..extra_lua_preset.script_file)end
