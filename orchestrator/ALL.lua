83648a8abad82ccd6a629700009c0d27e9b210fb
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 15:14:38 +0700

    Update ALL.lua

--- orchestrator/ALL.lua
+++ orchestrator/ALL.lua
@@ -1,5 +1,5 @@
 module("extra_lua_preset")
-extra_lua_preset.fileroot="addons/extra_lua_preset/presets"
+extra_lua_preset.fileroot="addons/extra_lua_preset/presets/"
 extra_lua_preset.presets={
 "variant_safeguard"
 "extra_math"
