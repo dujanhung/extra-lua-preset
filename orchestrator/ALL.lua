4fc8ec4cb81b3b05b2644d0ad812b6ebcd689c72
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 15:20:07 +0700

    Update ALL.lua

--- orchestrator/ALL.lua
+++ orchestrator/ALL.lua
@@ -1,8 +1,8 @@
 module("extra_lua_preset")
 extra_lua_preset.file_root="addons/extra_lua_preset/presets/"
 extra_lua_preset.presets={
-"safeguards/variant_safeguard/"
-"maths/extra_math/"
+"safeguards/variant_safeguard/",
+"maths/extra_math/",
 "variants/vector/"
 }extra_lua_preset.script_file="script.lua"
 for i in extra_lua_preset.presets do dofile(extra_lua_preset.file_root..extra_lua_preset.presets[i]..extra_lua_preset.script_file)end
