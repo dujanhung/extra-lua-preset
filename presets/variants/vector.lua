10260e86250fd98546ebff128edd15db4c23f17e
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 14:04:15 +0700

    Update vector.lua

--- presets/variants/vector.lua
+++ presets/variants/vector.lua
@@ -1,1 +1,14 @@
-.
+function vector.add(a,b)if!variant_safeguard.is_vector_stable({a,b})then return end local output={};for i=1,#a do table.insert(output,a[i]+b[i])end return output end
+function vector.subtract(a,b)if!variant_safeguard.is_vector_stable({a,b})then return end local output={};for i=1,#a do table.insert(output,a[i]-b[i])end return output end
+function vector.multiply(a,b)if!variant_safeguard.is_vector_stable({a,b})then return end local output={};for i=1,#a do table.insert(output,a[i]*b[i])end return output end
+function vector.divide(a,b)if!variant_safeguard.is_vector_stable({a,b})then return end local output={};for i=1,#a do table.insert(output,a[i]/b[i])end return output end
+function vector.scale(a,n)if!variant_safeguard.is_vector(a)then return end if!variant_safeguard.is_number(n)then return end local output={};for i=1,#a do table.insert(output,a[i]*n)end return output end
+function vector.exp(a,n)if!variant_safeguard.is_vector(a)then return end if!variant_safeguard.is_number(n)then return end local output={};for i=1,#a do table.insert(output,a[i]^n)end return output end
+function vector.mixdown(o)if!safegruard.is_vector(o)then return end local output=o[1];if#a>1 then for i=2,#a do output=output+o[i]end end return output end
+function vector.inverse(o)return vector.scale(o,-1)end
+function vector.exp2(o)return vector.exp(o,2)end
+function vector.length(o)return math.sqrt(vector.mixdown(vector.exp2(o)))end
+function vector.distance(a,b)return math.sqrt(vector.mixdown(vector.exp2(vector.subtract(a,b))))end
+function vector.dot(a,b)return vector.mixdown(vector.multiply(a,b))end
+function vector.normalize(o)return vector.divide(o,vector.length(o))end
+function vector.projection(a,b)local o=vector.normalized(b);return vector.scale(o,vector.dot(a,o))end
