36426c7b399ddb33eb0fbfaead45fc09664c4896
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 14:58:34 +0700

    Update script.lua

--- presets/maths/extra_math/script.lua
+++ presets/maths/extra_math/script.lua
@@ -1,3 +1,4 @@
+module("extra_math")
 extra_math.epsilon=1e-9
 function extra_math.is_approx(value,target,artifact)local o=value-target;if o>=-artifact and o<=artifact then return true end return false end
 function extra_math.is_zero_approx(value)if extra_math.is_approx(value,0,extra_math.epsilon)then return true end return false end
