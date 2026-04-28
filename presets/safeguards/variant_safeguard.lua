fc7430b15bfe27b7e0ad7a8f34b1850df16850b4
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 14:03:07 +0700

    Update variant_safeguard.lua

--- presets/safeguards/variant_safeguard.lua
+++ presets/safeguards/variant_safeguard.lua
@@ -1,1 +1,7 @@
-.
+function variant_safeguard.is_boolean(o)if type(o)!="boolean"then return false end return true end
+function variant_safeguard.is_number(o)if type(o)!="number"then return false end return true end
+function variant_safeguard.is_string(o)if type(o)!="string"then return false end return true end
+function variant_safeguard.is_table(o)if type(o)!="table"then return false end return true end
+function variant_safeguard.is_function(o)if type(o)!="function"then return false end return true end
+function variant_safeguard.is_vector(o)if!variant_safeguard.is_table(o)then return false end if!#o>0 then return false end for i=1,#o do if!variant_safeguard.is_number(a[i])then return false end end return true end
+function variant_safeguard.is_vector_stable(o)if!variant_safeguard.is_table(o)then return false end for i=1,#o do if!variant_safeguard.is_vector(o[i])then return false end if i>1 then if#o[1]!=#o[i]then return false end end end return true end
