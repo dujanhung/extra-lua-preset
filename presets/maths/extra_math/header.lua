41a48dc69af629c5b7e3a3bfb5d4bf9aa14b5ba4
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 14:34:06 +0700

    Update header.lua

--- presets/maths/extra_math/header.lua
+++ presets/maths/extra_math/header.lua
@@ -1,1 +1,14 @@
-.
+module(
+ "extra_math"
+)
+local config_filepath="extra-lua-preset/configs/extra_math.txt"
+local line_count=1
+for line in io.lines(config_filepath)do
+ if line_count==1 then
+  extra_math.epsilon=tonumber(line)
+ end
+ line_count=line_count+1
+end
+dofile(
+ "extra-lua-preset/maths/extra_math.lua"
+)
